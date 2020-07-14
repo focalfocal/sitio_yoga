c7306a611f949e93710391cbf386efcce1c399e9
jul <jul@jul.com>
2020-07-14 08:01:36 -0300

    luego de eliminado todo slugs. Todo funciona

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 from django.urls import reverse
-from django.utils.text import slugify
 
 # Create your models here.
 class Post(models.Model):
@@ -11,26 +10,13 @@ class Post(models.Model):
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
-    slug = models.SlugField(blank=True, default='')
 
     def publish(self):
         self.published_date = timezone.now()
-        #Agregada 1 linea para slug, siguiendo Easy2. Pero no anda.
-        #self.slug = slugify(self.title)
         self.save()
 
     def __str__(self):
         return self.title
-
-    #Agregado para slug, siguiendo Easy2
-    #Conviene separado de publish, para cuando haga drafts.
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
-        super(Post, self).save()
-
-    #Para uso con slug , al reves.
-    def get_absolute_url(self):
-        return reverse('detail', args=[str(self.slug)])
 
 class Comment(models.Model):
     post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
--- blog/views.py
+++ blog/views.py
@@ -8,11 +8,8 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
-#def post_detail(request, pk):
-#    post = get_object_or_404(Post, pk=pk)
-#    return render(request, 'blog/post_detail.html', {'post': post})
-def post_detail(request, slug=None):
-    post = get_object_or_404(Post, slug=slug)
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_new(request):
@@ -24,6 +21,7 @@ def post_new(request):
             post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
+            
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
@@ -41,6 +39,7 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
 
 def add_comment_to_post(request, pk):
     post = get_object_or_404(Post, pk=pk)
