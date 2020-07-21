618f1f4b20711c9aafbc3073162374f359f2a05e
jul <jul@jul.com>
2020-07-21 18:36:11 -0300

    added blog pagination

--- blog/views.py
+++ blog/views.py
@@ -6,11 +6,31 @@ import json
 import urllib
 from django.conf import settings
 from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    #posts_list = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    #paginator = Paginator(post_list, 2) # 2 posts in each page
+
+    #No cambiar el nombre a object_list porque falla
+    object_list = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    paginator = Paginator(object_list, 2) # 3 posts in each page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+    # If page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+    # If page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+    return render(request,
+        'blog/post_list.html',
+        {'page': page,
+        'posts': posts})
+
+    #return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
